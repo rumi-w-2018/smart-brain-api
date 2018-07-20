71076b37f1a200a1934e6343a3540723ce48136d
Harumi Warner <harumiwarner@gmail.com>
2018-07-20 17:01:28 -0600

    updated database connection for heroku

--- components/helpers/database.js
+++ components/helpers/database.js
@@ -4,10 +4,16 @@ require('dotenv').config();
 const db = knex({
   client: 'pg',
   connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+
+    /* 
+    //Local connection
     host: '127.0.0.1', // local host
     user: process.env.NODE_ENV_DB_USER,
     password: process.env.NODE_ENV_DB_PASS,
     database: process.env.NODE_ENV_DB_NAME
+  */
   }
 });
 
